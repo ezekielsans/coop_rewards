7ca415e5619532b93cc2eb7a9ddf05bfb9b2a2f8
Ezekiel Santos <128892368+ezekielsans@users.noreply.github.com>
2024-01-11 13:50:11 +0800

    Create sql_config.sql

---
+++ sql_config.sql
@@ -0,0 +1,72 @@
+CREATE TABLE coop_rewards_member
+(
+member_id character varying,
+amount numeric,
+date date,
+service_id integer,
+FOREIGN KEY(service_id) REFERENCES coop_rewards_services (service_id)
+)
+----------------------------------------------------------------------------
+CREATE TABLE coop_rewards_non_member
+(
+non_member_id character varying,
+amount numeric,
+date date,
+service_id integer,
+FOREIGN KEY(service_id) REFERENCES coop_rewards_services (service_id)
+)
+
+
+----------------------------------------------------------------------------
+SELECT * FROM 
+coop_rewards_services
+
+
+      REFERENCES public.coop_sub_major_occu (sub_major_code) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+
+---------------------------------------------------------------------------------------------
+/*
+FUNCTION FOR SUBMIT BUTTON
+*/
+
+  
+CREATE OR REPLACE FUNCTION submit_coop_rewards(acctno CHARACTER VARYING,amount NUMERIC,service_id_ CHARACTER VARYING)
+RETURNS CHARACTER VARYING AS
+$BODY$
+
+DECLARE
+acctno character varying;
+
+ins_acctno character varying;
+ins_amount character varying;
+ins_date character varying;
+ins_service_id character varying;
+
+
+
+BEGIN
+
+IF acctno NOT IN (SELECT cm.mem_id_no
+				  FROM coop_member  cm
+				  JOIN coop_associate ca 
+				  ON cm.mem_id_no = ca.reg_mem_no 
+				  WHERE cm.mem_id_no := ins_acctno)
+		  THEN 
+		  
+		  INSERT INTO coop_rewards_non_member (non_member_id,amount,date,service_id)
+		  VALUES (ins_acctno,ins_amount,SELECT returncustomdate1(),ins_service_id);
+		  
+		  ELSE
+		  
+		  INSERT INTO coop_rewards_member (member_id,amount,date,service_id)
+		  VALUES (ins_acctno,ins_amount,SELECT returncustomdate1(),ins_service_id);
+		  
+		  END IF;
+		  
+		  RETURN 'Account added'
+
+
+END
+$BODY$
+ LANGUAGE plpgsql 
