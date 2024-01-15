d4d1d86faf69ba2fb3cd0b06d3a4c9729db6209a
Ezekiel Santos <128892368+ezekielsans@users.noreply.github.com>
2024-01-15 15:07:06 +0800

    Create CoopRewardsInputModel.java

---
+++ CoopRewardsInputModel.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author mis
+ */
+@ManagedBean
+@SessionScoped
+public class CoopRewardsInputModel implements serializable {
+
+    private Integer serviceId;
+    private String serviceValue;
+    private String accountNo;
+    private BigDecimal amount;
+
+    public CoopRewardsInputModel() {
+    }
+
+    public Integer getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Integer serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getServiceValue() {
+        return serviceValue;
+    }
+
+    public void setServiceValue(String serviceValue) {
+        this.serviceValue = serviceValue;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+}
